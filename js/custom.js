0a21871f917da145551e9c56c1d94070a947a364
zhaohaihao <zhh2933@gmail.com>
2020-04-02 00:19:06 +0800

    Site updated: 2020-04-02 00:19:06

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// 百度统计
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?41c62e8338d3366d401ad616c607cd06";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
