894bd8f50e21dc1566ab3440cc8575c321fdb738
zhaohaihao <zhh2933@gmail.com>
2020-04-02 04:37:49 +0800

    Site updated: 2020-04-02 04:37:48

--- js/custom.js
+++ js/custom.js
@@ -20,3 +20,16 @@ var _hmt = _hmt || [];
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(bp, s);
 })();
+
+$(document).ready(function() {
+  // 集成fancybox, 为文章中所有的img元素添加父元素
+  $(".post-content img").each(function () {
+      if ($(this).hasClass('nofancybox')) return;
+
+     // $(this).attr("data-fancybox", "gallery"); 直接给img添加data-fancybox会导致点击图片后图片消失
+      var element = document.createElement("a");
+      $(element).attr("data-fancybox", "gallery");
+      $(element).attr("href", $(this).attr("src"));
+      $(this).wrap(element);
+  });
+});
